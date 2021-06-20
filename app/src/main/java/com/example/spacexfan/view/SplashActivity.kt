c4b0e8abd75095f726b3de9fd6f0bf0662d09f0d
Oğuzhan Yıldırım <oguzhan.yildirim@sbm.org.tr>
2021-06-20 11:39:15 +0300

    Logo & Splash screen finished.

--- app/src/main/java/com/example/spacexfan/view/SplashActivity.kt
+++ app/src/main/java/com/example/spacexfan/view/SplashActivity.kt
@@ -6,15 +6,17 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.spacexfan.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         val animation : Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.blink)
-        findViewById<TextView>(R.id.splash_text).startAnimation(animation)
+        splash.startAnimation(animation)
 
         @Suppress("DEPRECATION") val mHandler = Handler()
         val monitor = Runnable {
@@ -23,7 +25,7 @@ class SplashActivity : AppCompatActivity() {
             finish()
         }
 
-        mHandler.postDelayed(monitor, 2500)
+        mHandler.postDelayed(monitor, 1200)
 
     }
 }
